6fff24da488f3b7a991c088242c63d0a285c258a
Harendra Singh <harendra.acts@gmail.com>
2021-08-08 18:29:19 +1000

    Initial check-in 3

--- src/Test.java
+++ src/Test.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
@@ -6,5 +7,6 @@ public class Test {
     public static void main(String[] args) {
         List list = new ArrayList();
         List listAnimal2 = new ArrayList();
+        Date date = new Date();
     }
 }
