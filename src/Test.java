245daa8cf78b0a4f0f3597df93c61c4a95d460f7
Harendra Singh <harendra.acts@gmail.com>
2021-08-08 18:44:58 +1000

    Initial check-in 4

--- src/Test.java
+++ src/Test.java
@@ -7,6 +7,8 @@ import java.util.Set;
 public class Test {
     public static void main(String[] args) {
         List list = new ArrayList();
+        List listAnimal5 = new ArrayList();
+        Date date5 = new Date();
         List listAnimal2 = new ArrayList();
         Date date = new Date();
     }
