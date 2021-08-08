94fe83908c2d7c26e676c6fcffd638cb44b67bb3
Harendra Singh <harendra.acts@gmail.com>
2021-08-08 18:09:20 +1000

    Initial check-in

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class Test {
+    public static void main(String[] args) {
+        List list = new ArrayList();
+        List listAnimal2 = new ArrayList();
+    }
 }
