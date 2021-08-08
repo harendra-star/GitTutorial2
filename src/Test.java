77fb44a6b11c8d78deb8761c726377913513b2cb
Harendra Singh <harendra.acts@gmail.com>
2021-08-08 18:10:52 +1000

    Initial check-in

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,11 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class Test {
+    public static void main(String[] args) {
+        Set set = new HashSet();
+        List list =  new ArrayList();
+    }
 }
